8260fe22b0774528966f72d53ff31a6de5377ce0
mhdyhn <mhdyhnyfh2002@gmail.com>
2026-09-15 01:22:54 +0330

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: "/Sabze",
+
+  assetPrefix: "/Sabze/",
+
   images: {
     unoptimized: true,
   },
-  basePath: "/Sabze",
-  assetPrefix: "/Sabze/",
 };
 
 export default nextConfig;
