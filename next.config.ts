78e0a0870afd554bf649a0130076c6cef3a74ae8
mhdyhn <mhdyhnyfh2002@gmail.com>
2026-09-15 16:54:42 +0330

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/Sabze",
+  assetPrefix: "/Sabze/",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
+
+export default nextConfig;
